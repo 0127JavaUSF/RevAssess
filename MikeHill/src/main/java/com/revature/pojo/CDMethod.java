7e9231c9cedaecf06c1a8af5a4910e3d08e5903b
MRHill41188 <michael.hill.gd@outlook.com>
2020-02-17 14:33:44 -0500

    hopefully this is what your looking for

--- MikeHill/src/main/java/com/revature/pojo/CDMethod.java
+++ MikeHill/src/main/java/com/revature/pojo/CDMethod.java
@@ -4,22 +4,30 @@ public class CDMethod {
 
 	
 	public static int sum = 0;
+	public static int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 }; 
 	
 	public static void main(String[] args) {
-	 int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 }; 
-		 
- {
-
-		 for (int i=0; i<intArray.length; i++) {
-			 
-			 sum += intArray[i];
-
-	
-		 }		
-		 System.out.println(sum);
+	 
+		 calculate(intArray);
 		
-	}
 
+
+	}
+	
+	public static void calculate(int [] intArray) {
+		
+		 {
+
+			 for (int i=0; i<intArray.length; i++) {
+				 
+				 sum += intArray[i];
+
+		
+			 }		
+			 System.out.println(sum);
+			 sum = 0;
+			
+		}
 	}
 	
 
