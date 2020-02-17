d29fd9cbd651e22268f5fdaef4c18aa6ee72e10c
amikchowdhury <amikc1996@gmail.com>
2020-02-17 14:47:19 -0500

    finally fixed the github issues

---
+++ src/main/java/com/revature/pojo/IteratorSum.java
@@ -0,0 +1,21 @@
+package com.revature.pojo;
+
+public class IteratorSum {
+	static int iterateSum() {
+		int[] array = {1, 2, 3, 4, 5};
+		int sum = 0;
+
+		//Advanced for loop
+		for( int num : array) {
+		sum = sum+num;
+		}
+		System.out.println("Sum of the integers found in the array: "+sum);
+		return sum;
+		}         
+		   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/com/revature/pojo/User.java
+++ src/main/java/com/revature/pojo/User.java
@@ -6,27 +6,55 @@ private int id;
 private String firstName, lastName, username, password, role;
 
 
-static int iterateSum() {
-int[] array = {1, 2, 3, 4, 5};
-int sum = 0;
-
-//Advanced for loop
-for( int num : array) {
-sum = sum+num;
-}
-System.out.println("Sum of the integers found in the array: "+sum);
-return sum;
-}
-
-//public static int powerLengthRaiser() {
-//	int x;
-//	return 
-//}
-
-          
-   
-
 	
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String getRole() {
+	return role;
+}
+
+public void setRole(String role) {
+	this.role = role;
+}
+
 	public static void main(String[] args) {
 /* 		Create a method that iterates over an integer array and returns their sum.
 		Value: 30 points
@@ -34,7 +62,7 @@ return sum;
 		Create a method that, given an integer, can raise each digit to the power of the overall length and return whether the sum of the powers is equal to the original integer.
 		Value: 40 points
 */
-		iterateSum();
+
 	}
 
 }
