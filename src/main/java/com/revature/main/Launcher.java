dbc6ba591a4ba4e1ca645d2db6c3a17fc11f0a7a
charley43613 <41710701+charley43613@users.noreply.github.com>
2020-02-17 16:23:30 -0500

    give me 30 points please

--- src/main/java/com/revature/main/Launcher.java
+++ src/main/java/com/revature/main/Launcher.java
@@ -1,4 +1,9 @@
 package com.revature.main;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import utils.Computation;
 public class Launcher {
 
@@ -9,7 +14,21 @@ public class Launcher {
 		System.out.println("Sum of the result of the individual integers raised to the "
 				+ "power of the length equal to "
 				+ "original passed number is?: " + Computation.powerOfLength(1));
-		
+		try(Connection connection = utils.ConnectionUtil.getConnection()){
+			String sql = "SELECT * FROM bank_accounts";
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				System.out.println("Bank account name is " + rs.getString("bank_account_name") + " gil balance is: " + rs.getInt("gil_balance"));
+			}
+					
+					
+					
+					System.out.println();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
