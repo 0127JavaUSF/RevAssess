36fabd30262187af2396bf625e8f2ee1a96a196a
MRHill41188 <michael.hill.gd@outlook.com>
2020-02-17 14:18:26 -0500

    fixed method C

--- MikeHill/src/main/java/com/revature/pojo/CDMethod.java
+++ MikeHill/src/main/java/com/revature/pojo/CDMethod.java
@@ -3,7 +3,7 @@ package com.revature.pojo;
 public class CDMethod {
 
 	
-	public static int i = 11;
+	public static int sum = 0;
 	
 	public static void main(String[] args) {
 	 int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 }; 
@@ -11,9 +11,12 @@ public class CDMethod {
  {
 
 		 for (int i=0; i<intArray.length; i++) {
+			 
+			 sum += intArray[i];
 
-			 System.out.println(intArray[i]);
+	
 		 }		
+		 System.out.println(sum);
 		
 	}
 
