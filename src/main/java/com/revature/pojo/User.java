fcb1fe67d9c3a7fc14f86cfe763be2cb41e8f45d
clcollins1 <chrislcollins12@gmail.com>
2020-02-17 13:54:30 -0500

    tier one problem

---
+++ src/main/java/com/revature/pojo/User.java
@@ -0,0 +1,136 @@
+package com.revature.pojo;
+
+
+public class User {
+	int id;
+	String firstName, lastName, username, password, role;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	public User(int id, String firstName, String lastName, String username, String password, String role) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int arraySum(int[] array)
+	{
+		int sum = 0;
+		for(int i = 0; i < array.length; i++)
+		{
+			sum = array[i] + sum;
+		}
+		return sum;
+		
+	}
+	
+	public boolean sumOfDigitsPower(int num)
+	{
+		int digits = String.valueOf(num).length();
+		char[] digitArray = String.valueOf(num).toCharArray();
+		int sum = 0;
+		for (int i = 0; i < digitArray.length; i++)
+		{
+			sum = (int) (Math.pow(Character.getNumericValue(digitArray[i]), digits) + sum);
+		}
+		if (sum == num)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
